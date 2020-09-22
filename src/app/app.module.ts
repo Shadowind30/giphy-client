e231c61010aebd788273c7cd3c1994f2c45bbe13
Shadowind30 <shadowind30@gmail.com>
2020-09-22 00:43:14 -0400

    Added basic mobile layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GifItemComponent } from './gif-item/gif-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GifItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gif-item/gif-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gpc-gif-item',
+  templateUrl: './gif-item.component.html',
+  styleUrls: ['./gif-item.component.scss']
+})
+export class GifItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
